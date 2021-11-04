a400507a633d3a6b5442ccd4b9b3955fbae44830
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2021-11-04 13:10:22 +0200

    Ubung 4

---
+++ src/exercise4/MaxElement.java
@@ -0,0 +1,24 @@
+package exercise4;
+
+import java.util.Scanner;
+
+
+public class MaxElement {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter array length: ");
+        int size = input.nextInt();
+        double array[] = new double[size];
+        double maxValue = 2.2250738585072014E-308;
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter value for " + i + " element: ");
+            array[i] = input.nextDouble();
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+
+        }
+        System.out.println("Max number: "  + maxValue);
+    }
+}
---
+++ src/exercise4/MinIndex.java
@@ -0,0 +1,23 @@
+package exercise4;
+
+import java.util.Scanner;
+
+public class MinIndex {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter array length: ");
+        int size = input.nextInt();
+        double array[] = new double[size];
+        double minValue = 1.7976931348623157E308;
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter value for " + i + " element: ");
+            array[i] = input.nextDouble();
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+
+        }
+        System.out.println("Min index: "  + minValue);
+    }
+}
